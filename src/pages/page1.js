a219fdd7f23ce61dfe177a4d47877ebd5957a739
a-honey <a_honey___@hotmail.com>
2023-03-08 02:27:00 +0900

    - made My To Do List and one thing to do

---
+++ src/pages/page1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Helmet from 'react-helmet';
+// import styled from 'styled-components';
+
+export function page1() {
+    return (
+        <><Helmet>
+            <title>Page1</title>
+            <meta name="description" content="A todo page1"/>
+        </Helmet></>
+    )
+};
+export default page1;
